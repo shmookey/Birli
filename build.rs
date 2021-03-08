1e5eaf58175e91ce3afaf6c7b89fd23b888ab721
Derwent McElhinney <derwentx@gmail.com>
2021-03-08 15:20:45 +0800

    👷 CI uses aoflagger docker image

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 extern crate cxx_build;
-use std::mem::size_of_val;
 
 fn main() {
-    // Tell cargo to tell rustc to link the aoflagger
-    // shared library.
-    println!("cargo:rustc-link-lib=aoflagger");
+    println!("cargo:rustc-cfg=verbose");
 
     cxx_build::bridge("src/lib.rs")
         .flag_if_supported("-std=c++11")
@@ -14,12 +11,11 @@ fn main() {
         .file("src/cxx_aoflagger.cc")
         .compile("cxx_aoflagger");
 
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/cxx_aoflagger.cc");
     println!("cargo:rerun-if-changed=include/cxx_aoflagger.h");
 
-    // test memory layout
-
-    let usize_test: usize = 0;
-    println!("cargo:warning=usize_size={}", size_of_val(&usize_test));
+    // Tell cargo to tell rustc to link the aoflagger shared library.
+    println!("cargo:rustc-link-lib=dylib=aoflagger");
 }
