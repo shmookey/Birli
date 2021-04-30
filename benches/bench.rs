bc519efc0b38257dfb14fb537df6c5a0a9d4e3a0
Derwent McElhinney <derwentx@gmail.com>
2021-04-30 09:34:09 +0800

    ⚡️ add ord_half_1196175296 bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -71,27 +71,27 @@ fn get_context_mwax_half_1247842824() -> CorrelatorContext {
 //     CorrelatorContext::new(&metafits_path, &gpufits_files).unwrap()
 // }
 
-// fn get_context_ord_half_1196175296() -> CorrelatorContext {
-//     let test_dir = env::var("BIRLI_TEST_DIR").unwrap();
-//     let test_path = Path::new(&test_dir);
-//     let vis_path = test_path.join("1196175296_vis");
-//     let metafits_path = vis_path
-//         .join("1196175296.metafits")
-//         .to_str()
-//         .unwrap()
-//         .to_owned();
-//     let gpufits_glob = vis_path
-//         .join("1196175296_*gpubox*_00.fits")
-//         .to_str()
-//         .unwrap()
-//         .to_owned();
-//     let gpufits_files: Vec<String> = glob(gpufits_glob.as_str())
-//         .unwrap()
-//         .filter_map(Result::ok)
-//         .map(|path| path.to_str().unwrap().to_owned())
-//         .collect();
-//     CorrelatorContext::new(&metafits_path, &gpufits_files).unwrap()
-// }
+fn get_context_ord_half_1196175296() -> CorrelatorContext {
+    let test_dir = env::var("BIRLI_TEST_DIR").unwrap();
+    let test_path = Path::new(&test_dir);
+    let vis_path = test_path.join("1196175296_vis");
+    let metafits_path = vis_path
+        .join("1196175296.metafits")
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let gpufits_glob = vis_path
+        .join("1196175296_*gpubox*_00.fits")
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let gpufits_files: Vec<String> = glob(gpufits_glob.as_str())
+        .unwrap()
+        .filter_map(Result::ok)
+        .map(|path| path.to_str().unwrap().to_owned())
+        .collect();
+    CorrelatorContext::new(&metafits_path, &gpufits_files).unwrap()
+}
 
 // fn bench_context_to_baseline_imgsets_ord_half_1196175296(c: &mut Criterion) {
 //     let aoflagger = unsafe { cxx_aoflagger_new() };
@@ -105,6 +105,14 @@ fn bench_context_to_baseline_imgsets_mwax_half_1247842824(c: &mut Criterion) {
     let aoflagger = unsafe { cxx_aoflagger_new() };
     let context = get_context_mwax_half_1247842824();
     c.bench_function("context_to_baseline_imgsets - mwax_half_1247842824", |b| {
+        b.iter(|| context_to_baseline_imgsets(black_box(&aoflagger), black_box(&context)))
+    });
+}
+
+fn bench_context_to_baseline_imgsets_ord_half_1196175296(c: &mut Criterion) {
+    let aoflagger = unsafe { cxx_aoflagger_new() };
+    let context = get_context_ord_half_1196175296();
+    c.bench_function("context_to_baseline_imgsets - ord_half_1196175296", |b| {
         b.iter(|| context_to_baseline_imgsets(black_box(&aoflagger), black_box(&context)))
     });
 }
@@ -130,6 +138,7 @@ criterion_group!(
     config = Criterion::default().sample_size(10);
     targets =
         bench_context_to_baseline_imgsets_mwax_half_1247842824,
+        bench_context_to_baseline_imgsets_ord_half_1196175296,
         bench_flag_imgsets_mwax_half_1247842824
 );
 criterion_main!(benches);
